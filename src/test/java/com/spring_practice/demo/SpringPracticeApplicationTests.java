e537084ba0b534602ad0e7a0c4d49de6e1b4b56f
Chris Agold <christophersagold@gmail.com>
2021-03-27 11:38:42 -0500

    created student object

--- src/test/java/com/spring_practice/demo/SpringPracticeApplicationTests.java
+++ src/test/java/com/spring_practice/demo/SpringPracticeApplicationTests.java
@@ -1,6 +1,7 @@
 package com.spring_practice.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.bind.annotation.RestController;
 
